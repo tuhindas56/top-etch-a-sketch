e3886a115e533fb4abe7c177b86fc4fe0ddb082c
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-03-05 10:17:09 +0530

    Update UI design and include darkening hover effect in JS logic

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,49 @@
 "use strict";
 const pixelContainer = document.querySelector("#pixel-container");
-document.addEventListener("DOMContentLoaded", () => createGrid(16));
+const reset = new CustomEvent("reset", {
+    detail: {
+        red: 82,
+        green: 124,
+        blue: 129,
+    },
+});
 function createGrid(pixels) {
     for (let i = 0; i < pixels ** 2; i++) {
         const pixel = document.createElement("div");
-        pixel.style.height = `${640 / pixels}px`;
-        pixel.style.width = `${640 / pixels}px`;
+        pixel.style.height = `${500 / pixels}px`;
+        pixel.style.width = `${500 / pixels}px`;
         pixel.classList.add("pixel");
         pixelContainer.appendChild(pixel);
     }
     etchSketch();
 }
-function etchSketch() {
-    hoverTrail(document.querySelectorAll(".pixel"));
-    setupButtons();
-}
+document.addEventListener("DOMContentLoaded", () => createGrid(16));
 function hoverTrail(nodeList) {
-    nodeList.forEach((pixel) => pixel.addEventListener("mouseover", () => pixel.classList.add("pixel-hovered")));
+    nodeList.forEach((pixel) => {
+        let COLOR = {
+            red: 82,
+            green: 124,
+            blue: 129,
+        };
+        pixel.addEventListener("mouseover", () => {
+            COLOR.red -= 5;
+            COLOR.green -= 12;
+            COLOR.blue -= 12;
+            pixel.style.backgroundColor = `rgb(${COLOR.red}, ${COLOR.green}, ${COLOR.blue})`;
+        });
+        pixel.addEventListener("reset", (event) => {
+            COLOR.red = event.detail.red;
+            COLOR.green = event.detail.green;
+            COLOR.blue = event.detail.blue;
+        });
+    });
 }
 function setupButtons() {
-    const buttons = document.querySelector("#buttons");
+    const buttons = document.querySelector("#controls");
     const pixelList = document.querySelectorAll(".pixel");
     buttons.addEventListener("click", (event) => {
         let target = event.target;
+        const gridSize = document.querySelector("#grid-size");
         switch (target.id) {
             case "btn-grid-size":
                 const pixelInput = prompt("Enter number of pixels per side (Max 100 pixels)", "");
@@ -32,11 +53,19 @@ function setupButtons() {
                 else {
                     pixelList.forEach((pixel) => pixel.remove());
                     createGrid(parseInt(pixelInput));
+                    gridSize.textContent = `${pixelInput}x${pixelInput}`;
                 }
                 break;
             case "btn-reset":
-                pixelList.forEach((pixel) => pixel.classList.remove("pixel-hovered"));
+                pixelList.forEach((pixel) => {
+                    pixel.style.backgroundColor = "rgb(48, 72, 74)";
+                    pixel.dispatchEvent(reset);
+                });
                 break;
         }
     });
+}
+function etchSketch() {
+    hoverTrail(document.querySelectorAll(".pixel"));
+    setupButtons();
 }
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,54 @@
 const pixelContainer = document.querySelector("#pixel-container") as HTMLElement;
-document.addEventListener("DOMContentLoaded", () => createGrid(16));
+const reset = new CustomEvent("reset", {
+  detail: {
+    red: 82,
+    green: 124,
+    blue: 129,
+  },
+});
+
 function createGrid(pixels: number) {
   for (let i = 0; i < pixels ** 2; i++) {
     const pixel = document.createElement("div");
-    pixel.style.height = `${640 / pixels}px`;
-    pixel.style.width = `${640 / pixels}px`;
+    pixel.style.height = `${500 / pixels}px`;
+    pixel.style.width = `${500 / pixels}px`;
     pixel.classList.add("pixel");
     pixelContainer.appendChild(pixel);
   }
   etchSketch();
 }
 
-function etchSketch() {
-  hoverTrail(document.querySelectorAll(".pixel"));
-  setupButtons();
-}
+document.addEventListener("DOMContentLoaded", () => createGrid(16));
 
 function hoverTrail(nodeList: NodeListOf<Element>) {
-  nodeList.forEach((pixel) => pixel.addEventListener("mouseover", () => pixel.classList.add("pixel-hovered")));
+  nodeList.forEach((pixel) => {
+    let COLOR = {
+      red: 82,
+      green: 124,
+      blue: 129,
+    };
+    pixel.addEventListener("mouseover", () => {
+      COLOR.red -= 5;
+      COLOR.green -= 12;
+      COLOR.blue -= 12;
+      (pixel as HTMLElement).style.backgroundColor = `rgb(${COLOR.red}, ${COLOR.green}, ${COLOR.blue})`;
+    });
+    pixel.addEventListener("reset", (event) => {
+      COLOR.red = (event as CustomEvent).detail.red;
+      COLOR.green = (event as CustomEvent).detail.green;
+      COLOR.blue = (event as CustomEvent).detail.blue;
+    });
+  });
 }
 
 function setupButtons() {
-  const buttons = document.querySelector("#buttons") as HTMLElement;
+  const buttons = document.querySelector("#controls") as HTMLElement;
   const pixelList = document.querySelectorAll(".pixel");
+
   buttons.addEventListener("click", (event) => {
     let target = event.target as HTMLElement;
+    const gridSize = document.querySelector("#grid-size") as HTMLElement;
+
     switch (target.id) {
       case "btn-grid-size":
         const pixelInput = prompt("Enter number of pixels per side (Max 100 pixels)", "");
@@ -33,11 +57,20 @@ function setupButtons() {
         } else {
           pixelList.forEach((pixel) => pixel.remove());
           createGrid(parseInt(pixelInput));
+          gridSize.textContent = `${pixelInput}x${pixelInput}`;
         }
         break;
       case "btn-reset":
-        pixelList.forEach((pixel) => pixel.classList.remove("pixel-hovered"));
+        pixelList.forEach((pixel) => {
+          (pixel as HTMLElement).style.backgroundColor = "rgb(48, 72, 74)";
+          pixel.dispatchEvent(reset);
+        });
         break;
     }
   });
+}
+
+function etchSketch() {
+  hoverTrail(document.querySelectorAll(".pixel"));
+  setupButtons();
 }
