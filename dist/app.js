62dc3bc0d81f950fd76b4cca1fb3aec0edca79ce
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-03-12 20:48:26 +0530

    Refactor grid creation and resizing logic
    Revise the grid creation and resizing functions
    to improve efficiency and readability.
    Remove unnecessary code and added input
    validation for grid size. Update event
    listeners to handle grid resizing.

--- dist/app.js
+++ dist/app.js
@@ -2,14 +2,20 @@
 const pixelContainer = document.querySelector("#pixel-container");
 const buttons = document.querySelector("#controls");
 const currentGridSize = document.querySelector("#grid-size");
-document.addEventListener("DOMContentLoaded", () => createGrid("16"));
+let nodeList = document.querySelectorAll("#pixel-container > div");
+const mutationObserver = new MutationObserver(() => ((nodeList = document.querySelectorAll("#pixel-container > div")), hoverTrail()));
+mutationObserver.observe(pixelContainer, { childList: true });
+const reset = new CustomEvent("reset", {
+    detail: {
+        count: 1,
+    },
+});
 buttons.addEventListener("click", (click) => {
     let target = click.target;
-    console.log(target.id);
     switch (target.id) {
         case "btn-grid-size":
             let input = prompt("Enter number of pixels per side (Min: 2 pixels, Max: 100 pixels)", "");
-            if (!(input == null || input == "" || +input > 100 || +input < 2)) {
+            if (!(input == null || input == "" || +input > 100 || +input < 2 || input.match(/[\D]/g))) {
                 createGrid(input);
                 currentGridSize.textContent = `${input}x${input}`;
             }
@@ -17,17 +23,61 @@ buttons.addEventListener("click", (click) => {
                 alert("Cancelled.");
             }
             break;
+        case "btn-reset":
+            resetHighlight();
     }
 });
-function createGrid(getPixels) {
-    document.querySelectorAll("#pixel-container > div").forEach((div) => div.remove());
-    let pixels = +getPixels;
-    for (let i = 0; i < pixels ** 2; i++) {
-        let div = document.createElement("div");
-        div.style.height = `${500 / pixels}px`;
-        div.style.width = `${500 / pixels}px`;
-        div.classList.add("pixel");
-        pixelContainer.append(div);
+function createGrid(gridSize) {
+    let pixels = +gridSize;
+    resetHighlight();
+    if (nodeList.length > pixels ** 2) {
+        const toRemove = nodeList.length - pixels ** 2;
+        for (let i = 0; i < toRemove; i++) {
+            pixelContainer.removeChild(document.querySelector("#pixel-container > div"));
+        }
+        nodeList.forEach((node) => {
+            const div = node;
+            div.style.height = `${500 / pixels}px`;
+            div.style.width = `${500 / pixels}px`;
+            div.classList.add("pixel");
+        });
+    }
+    else {
+        const toAdd = pixels ** 2 - nodeList.length;
+        for (let i = 0; i < toAdd; i++) {
+            const div = document.createElement("div");
+            pixelContainer.append(div);
+        }
+        nodeList = document.querySelectorAll("#pixel-container > div");
+        nodeList.forEach((div) => {
+            div.style.height = `${500 / pixels}px`;
+            div.style.width = `${500 / pixels}px`;
+            div.classList.add("pixel");
+        });
     }
 }
-console.log(document.querySelectorAll("#pixel-container > div"));
+function hoverTrail() {
+    nodeList.forEach((div) => {
+        let count = 1;
+        div.addEventListener("mouseover", () => {
+            addHighlight(div, count);
+            count++;
+        });
+        div.addEventListener("reset", (event) => {
+            count = event.detail.count;
+        });
+    });
+}
+function addHighlight(element, count) {
+    element.classList.add(`hover-${count}`);
+}
+function resetHighlight() {
+    nodeList.forEach((div) => {
+        div.classList.value = "pixel";
+        div.dispatchEvent(reset);
+    });
+}
+document.addEventListener("DOMContentLoaded", () => {
+    createGrid("16");
+    hoverTrail();
+});
--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,88 @@
 const pixelContainer = document.querySelector("#pixel-container") as HTMLElement;
 const buttons = document.querySelector("#controls") as HTMLElement;
 const currentGridSize = document.querySelector("#grid-size") as HTMLElement;
-document.addEventListener("DOMContentLoaded", () => createGrid("16"));
+let nodeList = document.querySelectorAll("#pixel-container > div");
+const mutationObserver = new MutationObserver(
+  () => ((nodeList = document.querySelectorAll("#pixel-container > div")), hoverTrail())
+);
+mutationObserver.observe(pixelContainer, { childList: true });
+const reset = new CustomEvent("reset", {
+  detail: {
+    count: 1,
+  },
+});
 
 buttons.addEventListener("click", (click) => {
   let target = click.target as HTMLElement;
-  console.log(target.id);
   switch (target.id) {
     case "btn-grid-size":
       let input = prompt("Enter number of pixels per side (Min: 2 pixels, Max: 100 pixels)", "");
-      if (!(input == null || input == "" || +input > 100 || +input < 2)) {
+      if (!(input == null || input == "" || +input > 100 || +input < 2 || input.match(/[\D]/g))) {
         createGrid(input);
         currentGridSize.textContent = `${input}x${input}`;
       } else {
         alert("Cancelled.");
       }
       break;
+    case "btn-reset":
+      resetHighlight();
   }
 });
-function createGrid(getPixels: string) {
-  document.querySelectorAll("#pixel-container > div").forEach((div) => div.remove());
-  let pixels = +getPixels;
-  for (let i = 0; i < pixels ** 2; i++) {
-    let div = document.createElement("div");
-    div.style.height = `${500 / pixels}px`;
-    div.style.width = `${500 / pixels}px`;
-    div.classList.add("pixel");
-    pixelContainer.append(div);
+
+function createGrid(gridSize: string) {
+  let pixels = +gridSize;
+  resetHighlight();
+  if (nodeList.length > pixels ** 2) {
+    const toRemove = nodeList.length - pixels ** 2;
+    for (let i = 0; i < toRemove; i++) {
+      pixelContainer.removeChild(document.querySelector("#pixel-container > div")!);
+    }
+    nodeList.forEach((node) => {
+      const div = node as HTMLElement;
+      div.style.height = `${500 / pixels}px`;
+      div.style.width = `${500 / pixels}px`;
+      div.classList.add("pixel");
+    });
+  } else {
+    const toAdd = pixels ** 2 - nodeList.length;
+    for (let i = 0; i < toAdd; i++) {
+      const div = document.createElement("div");
+      pixelContainer.append(div);
+    }
+
+    nodeList = document.querySelectorAll("#pixel-container > div");
+    nodeList.forEach((div) => {
+      (div as HTMLElement).style.height = `${500 / pixels}px`;
+      (div as HTMLElement).style.width = `${500 / pixels}px`;
+      (div as HTMLElement).classList.add("pixel");
+    });
   }
 }
-console.log(document.querySelectorAll("#pixel-container > div"));
+function hoverTrail() {
+  nodeList.forEach((div) => {
+    let count = 1;
+    div.addEventListener("mouseover", () => {
+      addHighlight(div, count);
+      count++;
+    });
+    div.addEventListener("reset", (event) => {
+      count = (event as CustomEvent).detail.count;
+    });
+  });
+}
+
+function addHighlight(element: Element, count: number) {
+  element.classList.add(`hover-${count}`);
+}
+
+function resetHighlight() {
+  nodeList.forEach((div) => {
+    div.classList.value = "pixel";
+    div.dispatchEvent(reset);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  createGrid("16");
+  hoverTrail();
+});
